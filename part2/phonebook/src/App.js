23a2d01d6dbd94afd1c85fc535fffab30083169a
Tiffeny <tiffeny@pm.com>
2021-07-07 17:30:09 +0200

    add exercices 2.11 phonebook

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,19 +1,29 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import Filter from './components/Filter'
 import Persons from './components/Persons'
 import PersonForm from './components/PersonForm'
 
 const App = () => {
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122'}
-  ]) 
+
+  const [ persons, setPersons ] = useState([])
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
   const [ filteredPersonValue, setFilteredPersonValue ] = useState('')
-  const [ filteredPersons, setFilteredPersons ] = useState([...persons])
+  const [ filteredPersons, setFilteredPersons ] = useState([])
+  
+
+  // fetch data from json
+  useEffect(() => {
+    console.log('in effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+        setFilteredPersons(response.data)
+      })
+  }, [])
 
   const addPerson = (event) => {
     event.preventDefault()

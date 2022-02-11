9f576c3ecd1c69538b6f7a11adc89d98f098a513
Tiffeny <tiffeny@pm.com>
2022-02-11 17:03:11 +0100

    exercices 3.11

--- part3/index.js
+++ part3/index.js
@@ -3,7 +3,7 @@ const morgan = require('morgan')
 const cors = require('cors')
 const app = express()
 app.use(cors())
-app.use(express.static('build'))
+
 
 //app.use(morgan('tiny'))
 app.use(morgan(function (tokens, req, res) {

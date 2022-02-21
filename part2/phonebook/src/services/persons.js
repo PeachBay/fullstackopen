6f1588438c6d460b47953788a2c36f51c9e3d3e5
Tiffeny <tiffeny@pm.com>
2022-02-21 10:03:24 +0100

    3.11

--- part2/phonebook/src/services/persons.js
+++ part2/phonebook/src/services/persons.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'https://fullstackopen2021-part3.herokuapp.com/api/persons'
+const baseUrl = '/api/persons'
 
 const getAll = () => {
   const request = axios.get(baseUrl)

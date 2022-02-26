22ab3c545059c34637ab3941982b7e340245a85a
Tiffeny <6912905+PeachBay@users.noreply.github.com>
2022-02-26 14:54:23 +0100

    4.4

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -2,6 +2,15 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  const reducer = (sum, like) => {
+    return sum + like.likes
+  }
+
+  return blogs.reduce(reducer, 0)
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
